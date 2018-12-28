fa8e67b3ee6398d25d2e23e6df0234c04d67dd7f
lgh <lghwuwangchuxin@163.com>
2018-12-28 15:26:34 +0800

    提交

---
+++ src/main/java/com/js/site/log/manage/controller/LogSiteLogController.java
@@ -0,0 +1,21 @@
+package com.js.site.log.manage.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 采集站操作日志表 前端控制器
+ * </p>
+ *
+ * @author lgh
+ * @since 2018-12-21
+ */
+@Controller
+@RequestMapping("/logSiteLogModel")
+public class LogSiteLogController {
+
+}
+
---
+++ src/main/java/com/js/site/log/manage/mapper/LogSiteLogMapper.java
@@ -0,0 +1,16 @@
+package com.js.site.log.manage.mapper;
+
+import com.js.site.log.manage.model.LogSiteLogModel;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 采集站操作日志表 Mapper 接口
+ * </p>
+ *
+ * @author lgh
+ * @since 2018-12-21
+ */
+public interface LogSiteLogMapper extends BaseMapper<LogSiteLogModel> {
+
+}
